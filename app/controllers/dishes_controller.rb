bb000ee13b2820f705020c14f565673a875a3fec
Miguel Ferrer <miguef7@gmail.com>
2018-03-13 23:38:12 +0100

    add clasification for dishes to show properly on the view

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,5 +2,8 @@ class DishesController < ApplicationController
   def index
     @restaurant = Restaurant.find(params[:restaurant_id])
     @dishes = policy_scope(@restaurant.dishes)
+    @starters = @dishes.select { |dish| dish.category == "Starter"}
+    @mains = @dishes.select { |dish| dish.category == "Main"}
+    @desserts = @dishes.select { |dish| dish.category == "Dessert"}
   end
 end
