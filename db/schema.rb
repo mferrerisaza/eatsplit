1020cd93532e3cfb86a1253e23320919b4add5e1
Eric Haagensen <haagenseneric@yahoo.com>
2018-03-13 12:11:39 +0100

    fixing schema problem and adding seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312140711) do
+ActiveRecord::Schema.define(version: 20180313110850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20180312140711) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Restaurants
+
+restaurant_1 = Restaurant.new(
+  name: "Mario",
+  type: "Italian",
+  address: "Carrer Mallorca 212"
+)
+
+restaurant_1.remote_logo_url = "https://i.pinimg.com/originals/88/2d/88/882d883fcf289d704c064da27ed4fa60.png"
+restaurant_1.remote_photo_url = "https://www.cnet.com/i/bto/20091214/mario-pizza_610x457.jpg"
+
+restaurant_1.save!
+
+
