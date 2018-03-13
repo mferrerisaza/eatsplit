e18d86a521d1b6ad7f22046392e1b20e9d7558c7
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-13 19:19:07 +0100

    add helpers for photos and user name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def card_photo_or_generic(user)
+    if user.profile.nil? || user.profile.photo.blank?
+      cl_image_tag "facebook-profile-picture-no-pic-avatar.jpg", class: "table-dashboard-avatar"
+    else
+      cl_image_tag user.profile.photo, height: 393, width: 300, crop: :fill,gravity: :face, class: "table-dashboard-avatar"
+    end
+  end
+
+  def user_name_or_generic(user)
+    if user.profile.blank? == true
+      return "You"
+    else
+      return user.profile.name
+    end
+  end
+
 end
