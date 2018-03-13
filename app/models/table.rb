61f5e914f2824b3e6bbe2b86a306bf2eca2419e8
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-13 16:45:32 +0100

    add active bill methods

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,4 +2,16 @@ class Table < ApplicationRecord
   belongs_to :restaurant
   has_many :bills
   has_many :orders, through: :bills
+
+
+
+
+  def active_bill
+   self.bills.where.(status: "unpaid")
+  end
+
+  def active_bill?
+    active_bill.size == 1
+  end
+
 end
