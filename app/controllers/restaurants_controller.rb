c1c53b5f0db3fa8148438aead780808952d25d2a
Victor Gardrinier <v.gardrinier@gmail.com>
2018-03-14 10:50:14 +0100

    implement card html and css on the homepage. Add button scss file and card scss file. Display homepage on Iphone 7 screen. Create @city variable in restaurant controller for later

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = policy_scope(Restaurant).near("Carrer d'en grassot 101", 2)
+    @restaurants = policy_scope(Restaurant).near("Carrer d'en grassot 101", 1)
+    @city = request.location.city
   end
 end
