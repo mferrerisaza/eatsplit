effe908e72bace8875e3c0a3a79c840e332269a8
Miguel Ferrer <miguef7@gmail.com>
2018-03-14 12:54:41 +0100

    add routes for orders creation and checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "dishes/checkout", to: 'orders#checkout', as: :checkout
+
   resources :restaurants, only: :show do
-    resources :dishes, only: :index
+    resources :dishes, only: :index do
+      resources :orders, only: [:create, :update]
+    end
   end
 end
