828c93f2db947e4267dc9005a49026947ebf4360
Miguel Ferrer <miguef7@gmail.com>
2018-03-14 14:16:09 +0100

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   get "dishes/checkout", to: 'orders#checkout', as: :checkout
-
   resources :restaurants, only: :show do
     resources :dishes, only: :index do
       resources :orders, only: [:create, :update]
