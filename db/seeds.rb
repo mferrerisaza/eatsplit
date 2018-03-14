f2bf0e95a47f45f03aa19f17c8f5e705e603a2d7
Victor Gardrinier <v.gardrinier@gmail.com>
2018-03-14 18:06:01 +0100

    change logo of restaurants in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,7 @@ restaurant_1 = Restaurant.new(
   address: "Carrer Mallorca 212"
 )
 
-restaurant_1.remote_logo_url = "https://i.pinimg.com/originals/88/2d/88/882d883fcf289d704c064da27ed4fa60.png"
+restaurant_1.remote_logo_url = "https://www.le-ratelier.com/upload/pages-dynamiques/pdmqjxrc-couverts_couleur.png"
 restaurant_1.remote_photo_url = "https://media-cdn.tripadvisor.com/media/photo-s/0b/50/62/2c/the-restaurant-at-the.jpg"
 restaurant_1.user = User.find(1)
 
@@ -62,7 +62,7 @@ restaurant_2 = Restaurant.new(
   address: "Travessera de Gràcia 441"
 )
 
-restaurant_2.remote_logo_url = "https://i.pinimg.com/originals/88/2d/88/882d883fcf289d704c064da27ed4fa60.png"
+restaurant_2.remote_logo_url = "https://hillsboroughcheese.files.wordpress.com/2016/03/laplace_logowhite.png"
 restaurant_2.remote_photo_url = "https://kintamaniid-a903.kxcdn.com/wp-content/uploads/Hotel-The-One-Legian-4-1024x683.jpg"
 restaurant_2.user = User.find(1)
 
@@ -74,7 +74,7 @@ restaurant_3 = Restaurant.new(
   address: "Carrer Lepant 277"
 )
 
-restaurant_3.remote_logo_url = "https://i.pinimg.com/originals/88/2d/88/882d883fcf289d704c064da27ed4fa60.png"
+restaurant_3.remote_logo_url = "http://www.restaurant-la-croix-blanche.ch/images/sampledata/contact/logo-chef.png"
 restaurant_3.remote_photo_url = "http://www.lestresoms.com/_cache/resize/1920-744.crop/d2eb0d9965833944c5f8e3e9fe7c535a.jpg"
 restaurant_3.user = User.find(1)
 
@@ -86,7 +86,7 @@ restaurant_4 = Restaurant.new(
   address: "Carrer de València 334"
 )
 
-restaurant_4.remote_logo_url = "https://i.pinimg.com/originals/88/2d/88/882d883fcf289d704c064da27ed4fa60.png"
+restaurant_4.remote_logo_url = "http://logo-load.com/uploads/posts/2016-04/1459873183_texas-roadhouse-logo.png"
 restaurant_4.remote_photo_url = "https://ugc.zenchef.com/3/4/8/6/5/8/1/5/0/0/4/8/8/1500558718_293/b824a46e829f055b4ad94da613c66316.website.jpg"
 restaurant_4.user = User.find(1)
 
