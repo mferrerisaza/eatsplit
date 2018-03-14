319112c5baad798fc257da3c461deceb7f4eacc8
Miguel Ferrer <miguef7@gmail.com>
2018-03-14 12:50:20 +0100

    Modify create an orders actions to work with sessions orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  def create
+    dish = Dish.find(params[:dish_id])
+    order = Order.create!(dish: dish)
+    authorize order
+    session[:order_ids] = [] if session[:order_ids].blank?
+    session[:order_ids] << order.id
+  end
+
+  def checkout
+    @orders = session[:order_ids].map do |order_id|
+      Order.find(order_id)
+    end
+    @orders.each do |order|
+      authorize order
+    end
+  end
 end
