c3ff4b9b617e04a6ce896b18f5132cbf7c2b708d
Victor Gardrinier <v.gardrinier@gmail.com>
2018-03-14 12:16:53 +0100

    add private method to filter the query in the search form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = policy_scope(Restaurant).near("Carrer d'en grassot 101", 1)
-    @city = request.location.city
+    @restaurants = filter_by_name
+    @restaurants = policy_scope(@restaurants).near("carrer d'en grassot 101", 2)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+  end
+
+  private
+
+  def filter_by_name
+    if params[:query].present?
+
+      Restaurant.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      Restaurant.all
+    end
   end
 end
