3851889b2c9a1a4e5d7a57a7d03da0e03b54d20f
Victor Gardrinier <v.gardrinier@gmail.com>
2018-03-15 19:10:46 +0100

    schema adapted to new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313113612) do
+ActiveRecord::Schema.define(version: 20180315152605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180313113612) do
     t.integer "amount_cents", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "payment"
     t.index ["bill_id"], name: "index_orders_on_bill_id"
     t.index ["dish_id"], name: "index_orders_on_dish_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
