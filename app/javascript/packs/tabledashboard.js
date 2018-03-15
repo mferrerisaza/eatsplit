d53898ea6a2525240bcc686670c4e09454a861d8
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-16 00:59:54 +0100

    add code so that other switches toggle correctly

--- app/javascript/packs/tabledashboard.js
+++ app/javascript/packs/tabledashboard.js
@@ -3,6 +3,9 @@ const collapseMenus = document.querySelectorAll(".panel");
 collapseMenus.forEach(function(menu, index){
 
 menu.addEventListener("click", (event) => {
+  document.querySelectorAll(".tgl").forEach(function(toggle) {
+      toggle.checked = false
+  });
   let toggleSwitch = document.getElementById(`cb${index + 1}`);
   toggleSwitch.checked = !toggleSwitch.checked;
 });
