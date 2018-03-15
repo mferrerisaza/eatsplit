8acf0c2afc3354ad4c3866987a41d80f1450f23b
Miguel Ferrer <miguef7@gmail.com>
2018-03-15 19:38:58 +0100

    refactor order create method and add update method to add and remove quantity

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,35 @@
 class OrdersController < ApplicationController
   def create
     dish = Dish.find(params[:dish_id])
-    order = Order.create!(dish: dish)
-    authorize order
+    @order = Order.create!(dish: dish)
+    authorize @order
     session[:order_ids] = [] if session[:order_ids].blank?
-    session[:order_ids] << order.id
+    session[:order_ids] << @order.id
+    respond_to do |format|
+      format.html {}
+      format.js
+    end
+
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    authorize @order
+    if params["update"] == "plus"
+      @order.quantity += 1
+    elsif params["update"] == "minus"
+      @order.quantity -= 1
+    end
+    if @order.quantity == 0
+      @order.destroy
+    else
+      @order.save
+    end
+
+    respond_to do |format|
+      format.html {}
+      format.js
+    end
   end
 
   def checkout
