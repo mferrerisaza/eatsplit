b387e5b9bd9352eb5d6da1ce3aef8f102c3d9fe7
Miguel Ferrer <miguef7@gmail.com>
2018-03-16 09:41:47 +0100

    add default of 1 to quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313113612) do
+ActiveRecord::Schema.define(version: 20180315163158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20180313113612) do
     t.bigint "dish_id"
     t.bigint "bill_id"
     t.bigint "user_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.string "status", default: "pending"
     t.integer "amount_cents", default: 0, null: false
     t.datetime "created_at", null: false
