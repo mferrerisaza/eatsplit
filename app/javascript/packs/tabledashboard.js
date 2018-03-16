7c914c2f3df55020a0af20bccf308bfc95ef8a12
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-16 01:09:02 +0100

    fix toggle switch to work perfectly

--- app/javascript/packs/tabledashboard.js
+++ app/javascript/packs/tabledashboard.js
@@ -3,11 +3,13 @@ const collapseMenus = document.querySelectorAll(".panel");
 collapseMenus.forEach(function(menu, index){
 
 menu.addEventListener("click", (event) => {
-  document.querySelectorAll(".tgl").forEach(function(toggle) {
-      toggle.checked = false
-  });
   let toggleSwitch = document.getElementById(`cb${index + 1}`);
   toggleSwitch.checked = !toggleSwitch.checked;
+  document.querySelectorAll(".tgl").forEach(function(toggle) {
+      if (toggle !== toggleSwitch) {
+      toggle.checked = false
+      }
+  });
 });
 
 });
