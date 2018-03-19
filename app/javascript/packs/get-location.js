f05f3c2f86b3412c5961bbcbfe5be84812a5ca74
Miguel Ferrer <miguef7@gmail.com>
2018-03-19 14:43:07 +0100

    add js to handle location request

---
+++ app/javascript/packs/get-location.js
@@ -0,0 +1,12 @@
+function getUserLocation(){
+  navigator.geolocation.getCurrentPosition(function(position){
+    const array = [position.coords.latitude,position.coords.longitude];
+    console.log(array);
+  })
+  $.ajax({
+         url : `/location?data=${array}`,
+     });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  getUserLocation();
+})
