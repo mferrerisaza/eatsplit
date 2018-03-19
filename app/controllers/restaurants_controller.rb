3607ececb63de05bedb0c21a78931cc9429e5f7f
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-19 18:05:40 +0100

    add code for location path

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,9 @@
+# require "pry"
+
 class RestaurantsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :location]
   after_action :verify_authorized, except: [:index, :location]
+
   def index
     if !params[:query].present? && params[:query].nil?
       if session[:location].nil?
@@ -18,11 +21,14 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+
   end
 
   def location
     session[:location] = params[:data]
+    session[:jemoeder] = "Ollie"
     redirect_to root_path
+
   end
 
   private
