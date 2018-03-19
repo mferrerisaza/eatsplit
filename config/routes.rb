dbf9ba66517343220a21a5a6c99af8fe4d7ee2df
Miguel Ferrer <miguef7@gmail.com>
2018-03-19 14:44:16 +0100

    add route to location action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
   get "dishes/checkout", to: 'orders#checkout', as: :checkout
-  # put "orders/bill_update", to: 'orders#bill_update', as: :bill_update
+  get "/location", to: 'restaurants#location', as: :get_location
   resources :restaurants, only: :show do
     resources :dishes, only: :index do
       resources :orders, only: [:create, :update]
