b87bde6b4d08989cb9852c2d9df7c6b0856d2470
Victor Gardrinier <v.gardrinier@gmail.com>
2018-03-19 14:31:07 +0100

    add new root path and resources index

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   get "dishes/checkout", to: 'orders#checkout', as: :checkout
   # put "orders/bill_update", to: 'orders#bill_update', as: :bill_update
-  resources :restaurants, only: :show do
+  resources :restaurants, only: [:index, :show] do
     resources :dishes, only: :index do
       resources :orders, only: [:create, :update]
     end
   end
-  root to: 'restaurants#index'
+  root to: 'pages#landing_page'
   resources :tables, only: :show
     resources :bills, only: [:new, :show, :create, :update] do
       resources :payments, only: [:create]
