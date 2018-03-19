0ea071e873d27361394c096409ac2ceb76c9b18e
Eduardo Salazar <easalazarmiran@gmail.com>
2018-03-19 18:06:04 +0100

    add code for Geolocation Javascript API

--- app/javascript/packs/get-location.js
+++ app/javascript/packs/get-location.js
@@ -1,12 +1,28 @@
+const array = []
 function getUserLocation(){
   navigator.geolocation.getCurrentPosition(function(position){
-    const array = [position.coords.latitude,position.coords.longitude];
-    console.log(array);
+    const array = [position.coords.latitude, position.coords.longitude];
+    const url_data = `/location?data=${array}`;
+
+
+    $.ajax({
+           url : url_data,
+       });
   })
-  $.ajax({
-         url : `/location?data=${array}`,
-     });
 }
+
+
+
+
+
+// fetch(`/location?data="hello`, {
+//   method: 'GET',
+//   credentials: 'same-origin'
+// }).then((response) => {
+//   console.log(response)
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   getUserLocation();
 })
+
