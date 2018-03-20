674450f8d7f865dba76c6a44c63b63eb70161921
Miguel Ferrer <miguef7@gmail.com>
2018-03-20 18:34:55 +0100

    remove update method for the bill (now perform though the order itself)

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -15,22 +15,22 @@ class BillsController < ApplicationController
     authorize @bill
   end
 
-  def update
-    @bill = Bill.find(params[:id])
-    authorize @bill
-    @bill.update(bill_params)
-    @amount = @bill.check_orders
-    render json: {success: true}
-  end
+  # def update
+  #   @bill = Bill.find(params[:id])
+  #   authorize @bill
+  #   @bill.update(bill_params)
+  #   @amount = @bill.check_orders
+  #   render json: {success: true}
+  # end
 
   def create
     @table = Table.find(params[:table])
     redirect_to table_path(@table)
   end
 
-  private
+  # private
 
-  def bill_params
-    params.require(:bill).permit(order: [:id, :dish_id, :bill_id, :quantity, :status, :amount], orders_attributes: [:id, :dish_id, :bill_id, :quantity, :status, :amount])
-  end
+  # def bill_params
+  #   params.require(:bill).permit(order: [:id, :dish_id, :bill_id, :quantity, :status, :amount], orders_attributes: [:id, :dish_id, :bill_id, :quantity, :status, :amount])
+  # end
 end
