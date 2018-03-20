d50ee868cb07c7ee0daf634130e230a23e8e9af1
Miguel Ferrer <miguef7@gmail.com>
2018-03-20 13:17:02 +0100

    remove unnecesary comments and refactor without timer

--- app/javascript/packs/bill.js
+++ app/javascript/packs/bill.js
@@ -1,33 +1,3 @@
-// window.updateBill = function updateBill(event) {
-//   console.log(event.target);
-//   let val = document.getElementById("price-" + event.target.id).innerText;
-//   let val2 = parseFloat(val.slice(1));
-//   let total = document.getElementById("total");
-//   let totalnum = parseFloat(total.innerText);
-//   total.innerText = (totalnum + val2).toFixed(2);
-//   console.log(totalnum);
-// }
-
-
-// On a click of one of the checkboxes
-
-// Loop over the innerHTML's of the prices QuerySelectorAll
-// if statement, to see if checked,
-// outside of the loop, you have a counter
-// In the end, counter is the total
-
-
-function addListenerToBillies(){
-  const billies = document.querySelectorAll(".billy3:not(.paid)");
-  if (billies) {
-    billies.forEach(function(billy){
-      billy.addEventListener("click", (event) => {
-        setInterval(updateBill, 300);
-        // submit the form remotely with ajax
-      })
-    })
-  }
-}
 
 function addListenerToCheckboxes(){
   const checkboxes = document.querySelectorAll(".order-checkbox");
@@ -35,6 +5,7 @@ function addListenerToCheckboxes(){
     checkboxes.forEach(function(checkbox){
       checkbox.addEventListener("change", (event) => {
         Rails.fire(document.querySelector(".edit_bill"), 'submit');
+        updateBill();
       })
     })
   }
@@ -65,24 +36,10 @@ function updateBill() {
   document.getElementById("stripe-amount").value = counter * 100;
 }
 
-
-
-// }
-
-// function updatePaid(){
-//   if (document.getElementById('check').checked) {
-
-//   } else {
-
-//   }
-// }
-
-
-
 document.addEventListener("DOMContentLoaded",()=>{
   if (document.getElementById("bill-total-display")) {
     updateBill();
-    addListenerToBillies();
+    // addListenerToBillies();
     addListenerToCheckboxes();
   }
 })
