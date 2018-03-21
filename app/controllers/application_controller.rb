64aa1a5a2feed85b6e976e2e99ea782dbb71a8e7
Eric Haagensen <haagenseneric@yahoo.com>
2018-03-21 18:25:41 +0100

    override rails default behaviour when generating absolute url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,8 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 
 end
