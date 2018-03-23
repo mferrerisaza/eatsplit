a96a662a2d87d2cd8dc8eb3e2d30a89c147a3a9d
Miguel Ferrer <miguef7@gmail.com>
2018-03-23 10:31:28 +0100

    add create profile method before user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   has_many :restaurants
   has_many :orders
   has_many :bills, through: :orders
+  before_create :create_profile
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def create_profile
+    self.profile = Profile.create(name: self.email.split("@")[0])
+  end
 end
