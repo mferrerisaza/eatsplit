44d4e0cdb8198644332a8f26483380b14a9e52f3
Miguel Ferrer <miguef7@gmail.com>
2018-03-23 10:22:23 +0100

    tick your orders if are not alredy ticked

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -7,16 +7,13 @@ class BillsController < ApplicationController
     @your_orders = @bill.orders.where(user: current_user).where.not(status: "paid")
     @other_orders = @bill.orders.where.not(user: current_user).where.not(status: "paid")
     @paid_orders = @bill.orders.where(status: "paid")
-    # @bill.orders.where.not(status: "paid").each do |order|
-    #   if order.user == current_user
-    #     order.status = "ticked"
-    #     order.picking_user = current_user
-    #   else
-    #     order.status = "pending"
-    #     order.picking_user = nil
-    #   end
-    #   order.save!
-    # end
+    @bill.orders.where.not(status: "paid").where.not(status: "ticked").each do |order|
+      if order.user == current_user
+        order.status = "ticked"
+        order.picking_user = current_user
+      end
+      order.save!
+    end
     authorize @bill
   end
 
